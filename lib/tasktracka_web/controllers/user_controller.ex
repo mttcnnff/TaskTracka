9bace60b964c076a2f8585948cc5778c06ebd3df
Matthew Conniff <mvconniff@gmail.com>
2018-02-20 18:01:19 -0500

    Added task assign in cards

--- lib/tasktracka_web/controllers/user_controller.ex
+++ lib/tasktracka_web/controllers/user_controller.ex
@@ -4,10 +4,10 @@ defmodule TasktrackaWeb.UserController do
   alias Tasktracka.Accounts
   alias Tasktracka.Accounts.User
 
-  def index(conn, _params) do
-    users = Accounts.list_users()
-    render(conn, "index.html", users: users)
-  end
+  # def index(conn, _params) do
+  #   users = Accounts.list_users()
+  #   render(conn, "index.html", users: users)
+  # end
 
   def new(conn, _params) do
     changeset = Accounts.change_user(%User{})
@@ -25,29 +25,29 @@ defmodule TasktrackaWeb.UserController do
     end
   end
 
-  def show(conn, %{"id" => id}) do
-    user = Accounts.get_user!(id)
-    render(conn, "show.html", user: user)
-  end
+  # def show(conn, %{"id" => id}) do
+  #   user = Accounts.get_user!(id)
+  #   render(conn, "show.html", user: user)
+  # end
 
-  def edit(conn, %{"id" => id}) do
-    user = Accounts.get_user!(id)
-    changeset = Accounts.change_user(user)
-    render(conn, "edit.html", user: user, changeset: changeset)
-  end
+  # def edit(conn, %{"id" => id}) do
+  #   user = Accounts.get_user!(id)
+  #   changeset = Accounts.change_user(user)
+  #   render(conn, "edit.html", user: user, changeset: changeset)
+  # end
 
-  def update(conn, %{"id" => id, "user" => user_params}) do
-    user = Accounts.get_user!(id)
+  # def update(conn, %{"id" => id, "user" => user_params}) do
+  #   user = Accounts.get_user!(id)
 
-    case Accounts.update_user(user, user_params) do
-      {:ok, user} ->
-        conn
-        |> put_flash(:info, "User updated successfully.")
-        |> redirect(to: user_path(conn, :show, user))
-      {:error, %Ecto.Changeset{} = changeset} ->
-        render(conn, "edit.html", user: user, changeset: changeset)
-    end
-  end
+  #   case Accounts.update_user(user, user_params) do
+  #     {:ok, user} ->
+  #       conn
+  #       |> put_flash(:info, "User updated successfully.")
+  #       |> redirect(to: user_path(conn, :show, user))
+  #     {:error, %Ecto.Changeset{} = changeset} ->
+  #       render(conn, "edit.html", user: user, changeset: changeset)
+  #   end
+  # end
 
   def delete(conn, %{"id" => id}) do
     user = Accounts.get_user!(id)
